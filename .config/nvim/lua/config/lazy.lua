4a2d9eff6c7f95a75fb1564c2f8792152a931450
Prateek Ganguli <prateek.ganguli@gmail.com>
2024-05-22 13:00:27 +0530

    NVim updates

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,5 +1,6 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
@@ -30,7 +31,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  news = { lazyvim = false }, -- When enabled, NEWS.md will be shown when changed.
+  install = { colorscheme = { "habamax" } },
   checker = { enabled = false }, -- automatically check for plugin updates
   change_detection = { enabled = false }, -- automatically check for config file changes and reload the ui
   performance = {
--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -7,5 +7,4 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
 }
